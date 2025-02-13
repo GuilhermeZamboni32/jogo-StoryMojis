ba2c591a2569e24027ad64aac2725eda07c66936
GuilhermeZamboni32 <guilhermezamboni867@gmail.com>
2025-02-13 16:24:39 -0300

    botao-reset

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,20 +1,47 @@
 import React, { useState } from "react";
 import "./Story.css";
-import { Gi3dStairs, GiAlienBug, GiBeech, GiBee, GiBeachBall, GiBatwingEmblem, GiBattleMech, GiBigWave, GiCamel, GiCastle ,GiFoodChain } from "react-icons/gi";
+import { Gi3dStairs, GiAlienBug, GiBeech, GiBee, GiBeachBall, GiBatwingEmblem, GiBattleMech, GiBigWave, GiCamel, GiCastle, GiFoodChain, Gi3dHammer, GiAbstract097, GiAncientRuins, GiAncientSword, GiAnteater} from "react-icons/gi";
 
 function Story() {
   const [history, setHistory] = useState([]); 
+  const [RandomNumber, setRandomNumber] = useState([])
 
-  const icone = [
-    <Gi3dStairs />, <GiAlienBug />, <GiBeech />, <GiBee />, <GiBeachBall />,
-    <GiBatwingEmblem />, <GiBattleMech />, <GiBigWave />, <GiCamel />, <GiCastle />, <GiFoodChain />
-  ];
+  const [icone, setIcone] =useState([<Gi3dStairs />, <GiAlienBug />, <GiBeech />, <GiBee />, <GiBeachBall />,
+    <GiBatwingEmblem />, <GiBattleMech />, <GiBigWave />, <GiCamel />, <GiCastle />, <GiFoodChain />, <Gi3dHammer />, <GiAbstract097 />, <GiAncientRuins />, <GiAncientSword />, <GiAnteater />]);
+  const [iconeClone, setIconeClone] = useState([...icone])
 
-  const RandomNumber = () => {
-    const n = Math.floor(Math.random() * icone.length);
+//----------------------------------------------------------
+  const GeraRandomNumber = () => {
+    const n = Math.floor(Math.random() * iconeClone.length);
 
-    setHistory(prevHistory => [...prevHistory, icone[n]].slice(-5)); 
+  setRandomNumber(iconeClone [n])
+
+
+//----------------------------------------------------------
+    setIconeClone (prev => prev.filter((_, index) => index !== n))
+
+
+//----------------------------------------------------------
+    if (iconeClone.length === 0){
+      alert('já foram todos paisão')
+      return
+    }
+
+
+//----------------------------------------------------------
+    setHistory(prevHistory => [...prevHistory, iconeClone[n]].slice(-5));
   };
+
+
+//----------------------------------------------------------
+      const reset = () => {
+        setRandomNumber(null)
+        setIconeClone([...icone])
+
+  };
+
+
+//----------------------------------------------------------
 
   return (
     <div className="container-story">
@@ -24,10 +51,8 @@ function Story() {
         {/* Espaço entre os elementos */}
       </div>
 
-
-
       
-      <p>{history.map((emoji, index) => <span key={index}>{emoji} </span>)}</p>
+      <p>{history.map((iconeClone, index) => <span key={index}>{iconeClone} </span>)}</p>
 
 
 
@@ -35,9 +60,16 @@ function Story() {
         {/* Espaço entre os elementos */}
       </div>
 
-      <button onClick={RandomNumber}>Gerar emoji</button>
+      <button onClick={GeraRandomNumber}>Gerar emoji</button>
+      <button onClick={reset}>reinici</button>
+      
     </div>
   );
 }
 
 export default Story;
+
+
+
+/**<Gi3dStairs />, <GiAlienBug />, <GiBeech />, <GiBee />, <GiBeachBall />,
+    <GiBatwingEmblem />, <GiBattleMech />, <GiBigWave />, <GiCamel />, <GiCastle />, <GiFoodChain /> */
